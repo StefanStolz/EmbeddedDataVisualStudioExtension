507332c687f358dcdff51e2606a55c7ef176589d
Stefan Stolz <stefan.stolz@awin-software.de>
2016-06-24 11:51:08 +0200

    Project Structure

---
+++ EmbeddedData/embeddeddata.logic/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("embeddeddata.logic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("embeddeddata.logic")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("09dd7183-62a5-4bad-a04c-1233d8ad2c49")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("embeddeddata.logic.tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010087ec2297665d7988b5c69cbb9d08b8971ab178d225e65257eff6861c7e340a51affe29ae45bc234c6696f49bfd3f1833259199b956f185ab3736fe4aaa53fbd90ea1241ee0da6186ec3ee91a7bd844c2d28e3858a3a64bbf38974ff297037e5481dd856a2ee037097d056d5778f22270ed7608083b0212c8a80fd2846c3930a4")]
